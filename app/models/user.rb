73dd62b12a37ac782e879f060169af166093712d
naoki39 <naokibad0311@gmail.com>
2022-03-11 11:28:58 +0900

    モデルの設定

---
+++ app/models/user.rb
@@ -0,0 +1,18 @@
+class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
+
+  validates :password, format: { with: /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i }, on: :create
+
+  with_options presence: true do
+      validates :nickname
+      validates :goal
+      validates :second_goal
+      validates :to_do
+      validates :image
+  end
+  
+  has_one_attached :image
+end
