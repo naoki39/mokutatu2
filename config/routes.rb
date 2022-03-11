985ab98c3979dfca5808ce2c464256f749792153
naoki39 <naokibad0311@gmail.com>
2022-03-11 13:24:44 +0900

    postのルーティング設定

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users
   root to:"communities#index"
+  resources :communities, only: :index do
+    resources :posts, only: [:index, :new, :create, :destroy] 
+  end
+  resources :users, only: [:show, :edit, :update]
 end
