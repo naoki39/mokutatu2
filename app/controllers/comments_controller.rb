f5967a24ac2512177b5f3f81c378c5dcd0f6c013
naoki39 <naokibad0311@gmail.com>
2022-03-14 09:23:24 +0900

    コントローラーの作成

---
+++ app/controllers/comments_controller.rb
@@ -0,0 +1,29 @@
+class CommentsController < ApplicationController
+  before_action :authenticate_user!
+  before_action :set_comment, only: [:create, :destroy]
+
+  def create
+    set_comment
+    @comment = Comment.create(comment_params)
+    redirect_to "/posts/#{@post.id}"
+  end
+
+  def destroy
+    set_comment
+    @comment = Comment.find(params[:id])
+    if current_user.id == @comment.user.id
+      @comment.destroy 
+      redirect_to "/posts/#{@post.id}" 
+    end
+  end
+
+  private
+
+  def set_comment
+    @post = Post.find(params[:post_id])
+  end
+
+  def comment_params
+    params.require(:comment).permit(:text).merge(user_id: current_user.id, post_id: params[:post_id])
+  end
+end
