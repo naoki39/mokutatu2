ff8fef2cc57457b1b6a758d5e0a1967613edf1f6
naoki39 <naokibad0311@gmail.com>
2022-04-03 09:08:48 +0900

    記述追加

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -31,7 +31,7 @@ class PostsController < ApplicationController
     set_post#community_idを取得
     @post = Post.find(params[:id])#投稿を取得
     if current_user.id == @post.user.id#現在ログイン中ユーザーと投稿主が一致していた場合
-      @post.destroy 
+      @post.destroy #投稿の削除
       redirect_to community_posts_path(@community.id)
     end
   end
